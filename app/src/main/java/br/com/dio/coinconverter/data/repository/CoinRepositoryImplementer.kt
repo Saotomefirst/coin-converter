34544837de5a736526d8a6ca87436f0a5e806868
Saotomefirst <jorge.appleseed@gmail.com>
2022-02-05 22:23:57 -0300

    Tratamento de Erros
    Aproveitando a estrutura robusta do projeto, adicionamos um tratamento de erro para interpretar e exibir a mensagem de erro retornada pela consulta da API

--- app/src/main/java/br/com/dio/coinconverter/data/repository/CoinRepositoryImplementer.kt
+++ app/src/main/java/br/com/dio/coinconverter/data/repository/CoinRepositoryImplementer.kt
@@ -1,16 +1,30 @@
 package br.com.dio.coinconverter.data.repository
 
+import br.com.dio.coinconverter.core.exceptions.RemoteException
+import br.com.dio.coinconverter.data.model.ErrorResponse
 import br.com.dio.coinconverter.data.model.ExchangeResponse
 import br.com.dio.coinconverter.data.services.AwesomeService
+import com.google.gson.Gson
 import kotlinx.coroutines.flow.flow
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.HttpException
 
 class CoinRepositoryImplementer(
     private val service: AwesomeService
 ) : CoinRepository {
     override suspend fun getExchangeValue(coins: String) = flow {
-        val exchangeValue = service.exchangeValue(coins)
-        val exchange = exchangeValue.values.first()
-        // esse método é do coroutines
-        emit(exchange)
+        try{
+            val exchangeValue = service.exchangeValue(coins)
+            val exchange = exchangeValue.values.first()
+            // esse método é do coroutines
+            emit(exchange)
+        }
+        catch (e: HttpException) {
+            //{"status":404,"code":"CoinNotExists","message":"moeda nao encontrada USD-USD"}
+            val json = e.response()?.errorBody()?.string()
+            val errorResponse = Gson().fromJson(json, ErrorResponse::class.java)
+            throw RemoteException(errorResponse.message)
+        }
+
     }
 }
